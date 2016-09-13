e339119e4f9641e488611b69bc132afb02d60a70
Ewa Manek and Georgina Van Dort <ewa@>
2016-09-13 11:51:02 -0700

    add comparator method

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,6 +1,8 @@
 import java.util.HashMap;
 import java.util.Map;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import spark.ModelAndView;
 import spark.template.velocity.VelocityTemplateEngine;
 import static spark.Spark.*;
@@ -12,7 +14,19 @@ public class App {
 
     get("/", (request, response) -> {
       Map<String, Object> model = new HashMap<String, Object>();
-      model.put("teas", request.session().attribute("teas"));
+      ArrayList<Tea> teas = request.session().attribute("teas");
+      //check if teas null
+      //if not null, implement comparable using getters for whatever value we are comparing
+      //both strings and ints implement comparable, so can just use compareto
+      if(teas != null){
+        Collections.sort(teas, new Comparator<Tea>() {
+            @Override
+            public int compare(Tea tea1, Tea tea2) {
+                return tea1.getName().compareTo(tea2.getName());
+            }
+        });
+      }
+      model.put("teas", teas);
       model.put("template", "templates/index.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
